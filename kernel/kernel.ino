fe81c7a1569f62d5b48ee6c2c1f5a51287b0be4f
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2019-06-06 22:33:28 +0200

    Many things

--- kernel/kernel.ino
+++ kernel/kernel.ino
@@ -1,11 +1,13 @@
 /*
- * FantilatorOS
- */
+   FantilatorOS
+*/
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+// Temperature measurement
+#include <dht.h>
 
 // Predefined button states
 #define BUTTON_STATE_OFF 0
@@ -16,7 +18,7 @@
 // Delay for main loop in ms
 #define LOOP_DELAY 25
 // Delay before WAIT becomes HOLD in loops
-#define HOLD_THRESHOLD (500/LOOP_DELAY) 
+#define HOLD_THRESHOLD (300/LOOP_DELAY)
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -25,32 +27,61 @@
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-typedef struct{
-    int pin;
-    int state;
-    int holdCounter;
+#define DHT_PIN A0 // Temperature sensor
+#define DHT_DELAY (5000/LOOP_DELAY)
+
+#define PWM_PIN_LEFT 9
+#define PWM_PIN_RIGHT 10
+
+typedef struct {
+  uint8_t pin;
+  uint8_t state;
+  int holdCounter;
 } Button;
 
-// Digital pin 2 is left button
-Button buttonLeft = {2, BUTTON_STATE_OFF, 0};
-// Digital pin 3 is right button
-Button buttonRight = {3, BUTTON_STATE_OFF, 0};
-// Digital pin 4 is right button
-Button buttonUp = {4, BUTTON_STATE_OFF, 0};
+typedef struct {
+  String label;
+  // Qui peut être modifié
+  bool modifiable;
+  int8_t* value;
+  String suffix;
+} MenuItem;
 
-int currentPower = 0;
-int pwmPin = 9;
+// Digital pin 2 is down button
+Button buttonDown = {2, BUTTON_STATE_OFF, 0};
+// Digital pin 3 is up button
+Button buttonUp = {3, BUTTON_STATE_OFF, 0};
+// Digital pin 4 is enter button
+Button buttonEnter = {4, BUTTON_STATE_OFF, 0};
+
+// Temperature and humidity sensor
+dht DHT;
+
+uint16_t iteration = 0;
+// Current power percentage
+int8_t currentPowerLeft = 0;
+int8_t currentPowerRight = 0;
+int8_t currentPower = 0;
+
+int8_t currentMenuItem = 0;
+int8_t settingItemIndex = 0;
+
+int8_t temperature = 0;
+int8_t humidity = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // Make the button pins an input:
-  pinMode(buttonLeft.pin, INPUT);
-  pinMode(buttonRight.pin, INPUT);
+  pinMode(buttonDown.pin, INPUT);
   pinMode(buttonUp.pin, INPUT);
+  pinMode(buttonEnter.pin, INPUT);
   // Make the pwm pin as output
-  pinMode(pwmPin, OUTPUT);
+  pinMode(PWM_PIN_LEFT, OUTPUT);
+  pinMode(PWM_PIN_RIGHT, OUTPUT);
+
+  pinMode(DHT_PIN, INPUT);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
@@ -65,11 +96,12 @@ void setup() {
 
   // Clear the buffer
   display.clearDisplay();
-  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextSize(1); // Draw 2X-scale text
   display.setTextColor(WHITE);
 }
 
-Button updateButtonState(Button button, int newState) {
+Button updateButtonState(Button button) {
+  uint8_t newState = digitalRead(button.pin);
   if (newState == 0) {
     button.state = BUTTON_STATE_OFF;
     button.holdCounter = 0;
@@ -77,10 +109,10 @@ Button updateButtonState(Button button, int newState) {
     if (button.state == BUTTON_STATE_OFF) {
       button.state = BUTTON_STATE_PRESS;
     } else {
-      button.holdCounter++;
       if (button.holdCounter >= HOLD_THRESHOLD) {
         button.state = BUTTON_STATE_HOLD;
       } else {
+        button.holdCounter++;
         button.state = BUTTON_STATE_WAIT;
       }
     }
@@ -90,48 +122,141 @@ Button updateButtonState(Button button, int newState) {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input pin:
-  int newButtonLeftState = digitalRead(buttonLeft.pin);
-  int newButtonRightState = digitalRead(buttonRight.pin);
-  int newButtonUpState = digitalRead(buttonUp.pin);
+  bool displayRefresh = (iteration == 0);
+  iteration++;
 
-  buttonLeft = updateButtonState(buttonLeft, newButtonLeftState);
-  buttonRight = updateButtonState(buttonRight, newButtonRightState);
-  buttonUp = updateButtonState(buttonUp, newButtonUpState);
+  MenuItem menuItems[] = {{
+      .label = F("Speed"),
+      .modifiable = true,
+      .value = &currentPower,
+      .suffix = "%"
+    },
+    { .label = F("Speed left"),
+      .modifiable = true,
+      .value = &currentPowerLeft,
+      .suffix = "%"
+    },
+    { .label = F("Speed right"),
+      .modifiable = true,
+      .value = &currentPowerRight,
+      .suffix = "%"
+    },
+    { .label = F("Curr temp"),
+      .modifiable = false,
+      .value = &temperature,
+      .suffix = "\xF7\x43"
+    },
+    { .label = F("Humidity"),
+      .modifiable = false,
+      .value = &humidity,
+      .suffix = "%"
+    },
+    { .label = F("Build"),
+      .modifiable = false,
+      .value = 0,
+      .suffix = F(__DATE__)
+    }
+  };
+  int totalMenuItems = sizeof(menuItems) / sizeof(menuItems[0]);
 
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print("L: ");
-  if (buttonLeft.state == BUTTON_STATE_OFF) {
-    display.println("OFF");
-  } else if (buttonLeft.state == BUTTON_STATE_PRESS) {
-    display.println("CLICK");
-  } else if (buttonLeft.state == BUTTON_STATE_WAIT) {
-    display.println("WAIT");
-  } else {
-    display.println("HOLD");
-  }
-  display.print("R: ");
-  if (buttonRight.state == BUTTON_STATE_OFF) {
-    display.println("OFF");
-  } else if (buttonRight.state == BUTTON_STATE_PRESS) {
-    display.println("CLICK");
-  } else if (buttonRight.state == BUTTON_STATE_WAIT) {
-    display.println("WAIT");
-  } else {
-    display.println("HOLD");
-  }
-  display.print("U: ");
-  if (buttonUp.state == BUTTON_STATE_OFF) {
-    display.println("OFF");
-  } else if (buttonUp.state == BUTTON_STATE_PRESS) {
-    display.println("CLICK");
-  } else if (buttonUp.state == BUTTON_STATE_WAIT) {
-    display.println("WAIT");
-  } else {
-    display.println("HOLD");
+  buttonDown = updateButtonState(buttonDown);
+  buttonUp = updateButtonState(buttonUp);
+  buttonEnter = updateButtonState(buttonEnter);
+
+  if (iteration % DHT_DELAY == 0) {
+    DHT.read11(DHT_PIN);
+    temperature = DHT.temperature;
+    humidity = DHT.humidity;
+    displayRefresh = true;
   }
 
-  display.display();
+  int8_t valueAdjustment = 0;
+  if (buttonDown.state == BUTTON_STATE_PRESS || buttonDown.state == BUTTON_STATE_HOLD) {
+    valueAdjustment = -1;
+  }
+
+  if (buttonUp.state == BUTTON_STATE_PRESS || buttonUp.state == BUTTON_STATE_HOLD) {
+    valueAdjustment = 1;
+  }
+
+  if (buttonEnter.state == BUTTON_STATE_PRESS) {
+    if (settingItemIndex == 0) {
+      MenuItem *item = &menuItems[currentMenuItem];
+      if (item->modifiable) {
+        settingItemIndex = currentMenuItem + 1;
+      }
+    } else {
+      settingItemIndex = 0;
+    }
+    displayRefresh = true;
+  }
+
+  if (valueAdjustment != 0) {
+    if (settingItemIndex == 0) {
+      currentMenuItem = (totalMenuItems + currentMenuItem - valueAdjustment) % totalMenuItems;
+    } else {
+      MenuItem *item = &menuItems[settingItemIndex - 1];
+      *(item->value) += valueAdjustment;
+      if (*(item->value) < 0) {
+        *(item->value) = 0;
+      } else if (*(item->value) > 100) {
+        *(item->value) = 100;
+      }
+      if (settingItemIndex == 1) {
+        currentPowerLeft = currentPowerRight = currentPower;
+      } else if (settingItemIndex == 2 || settingItemIndex == 3) {
+        currentPower = *(item->value);
+      }
+    }
+    displayRefresh = true;
+  }
+
+  if (displayRefresh) {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.println("Fantilator");
+    display.setTextSize(1);
+    for (int i = 0; i < totalMenuItems; i++) {
+      if (i == currentMenuItem) {
+        display.setTextColor(BLACK, WHITE);
+        display.print("\x10");
+      } else {
+        display.setTextColor(WHITE);
+        display.print(" ");
+      }
+      int8_t printedChars = 1;
+      MenuItem *item = &menuItems[i];
+      display.print(item->label);
+      printedChars += item->label.length();
+      String value;
+      if (i == 0 && currentPowerLeft != currentPowerRight) {
+        value = "--";
+      } else if (item->value) {
+        value = String(*item->value);
+      } else {
+        value = "";
+      }
+      int8_t neededSpaces = 19 - printedChars - value.length() - item->suffix.length();
+      for (int8_t j = 0; j < neededSpaces; j++) {
+        display.print(" ");
+      }
+      if (i == settingItemIndex - 1) {
+        display.print("\x11");
+      } else {
+        display.print(" ");
+      }
+      display.print(value);
+      display.print(item->suffix);
+      if (i == settingItemIndex - 1) {
+        display.println("\x10");
+      } else {
+        display.println(" ");
+      }
+    }
+    display.display();
+  }
+
   delay(LOOP_DELAY);        // delay in between reads for stability
 }
